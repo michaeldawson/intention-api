6479cef9e1444758249765eb86e834512b365124
Michael Dawson <m.dawson@y7mail.com>
2019-04-24 15:36:38 +0200

    Add device id support to submissions endpoint

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,11 +1,21 @@
 class SubmissionsController < ApplicationController
   def create
-    submission = Submission.new(params[:submission])
+    uid = params[:device_id]
+    user = User.first_or_initialize(uid: uid)
+    user.update!(password: 'password', email: uid + "@actionpactapp.com")
+
+    submission = user.submissions.build(submission_params)
 
     if submission.save
       render json: { success: true }
     else
       render status: 400, json: { success: false, errors: submission.errors }
     end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:what_for, :why_now, :what_else)
   end
 end
--- spec/requests/submission_request_spec.rb
+++ spec/requests/submission_request_spec.rb
@@ -1,18 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe "Widget management", type: :request do
-  it "creates a Widget and redirects to the Widget's page" do
-    params = {
+RSpec.describe "Posting submissions", type: :request do
+  let(:params) {
+    {
       submission: {
         what_for: "Checking email",
         why_now: "I check email at midday",
         what_else: "I could be meditating"
-      }
+      },
+      device_id: 'abc123'
     }
-    post "/submissions", params: params
+  }
 
-    expect(response).to be_successful
+  context 'with a new device id' do
+    it 'creates a new user and creates a submission record for that user' do
+      expect {
+        post "/submissions", params: params
+      }.to change {
+        User.count
+      }.by(1)
 
-    binding.pry
+      user = User.last
+      expect(user.uid).to eq('abc123')
+      expect(user.email).to eq('abc123@actionpactapp.com')
+
+      submission = user.submissions.last
+      expect(submission.what_for).to eq("Checking email")
+      expect(submission.why_now).to eq("I check email at midday")
+      expect(submission.what_else).to eq("I could be meditating")
+
+      response_body = JSON.parse(response.body)
+      expect(response_body['errors']).to be_blank
+    end
+  end
+
+  context "with a device id that's been used before" do
+    before { post "/submissions", params: params }
+
+    it 'creates a new submission for that existing user, without creating a new user' do
+      expect {
+        post "/submissions", params: params
+      }.not_to change {
+        User.count
+      }
+
+      user = User.first
+      expect(user.submissions.count).to eq(2)
+    end
   end
 end
